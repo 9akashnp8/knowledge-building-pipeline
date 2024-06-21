6b608025c582cc013c74166468a2428e038f82ab
Akash NP <akashnp1998@gmail.com>
2024-06-21 23:31:19 +0400

    chore: make highlights selectable via date

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,34 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, ChangeEvent, Dispatch, SetStateAction } from 'react'
 
 import type { HighlighAPIResponse } from './types'
 
+type Props = {
+  highlights: HighlighAPIResponse,
+  setSelectHighlights: Dispatch<SetStateAction<any>>
+}
+
+function SelectHighlightsDate({ highlights, setSelectHighlights }: Props) {
+
+  function handleChange(e: ChangeEvent) {
+    e.preventDefault()
+    const selectedDate = e.target.value;
+    setSelectHighlights(highlights[selectedDate])
+  }
+
+  return (
+    <select onChange={handleChange}>
+      <option value="">Select Date</option>
+      {Object.keys(highlights).map((date) => {
+        return <option value={date}>{date}</option>
+      })}
+    </select>
+  )
+}
+
 function App() {
   const [highlights, setHighlights] = useState<HighlighAPIResponse | []>([])
-
+  const [selectHighlights, setSelectHighlights] = useState<Highlight | []>([])
+  
   useEffect(() => {
     async function getHighlights() {
       fetch("http://localhost:8000/highlights?file_path=x")
@@ -17,16 +41,14 @@ function App() {
 
   return (
     <>
-      {Object.entries(highlights).map(([key, value]) => {
+      <SelectHighlightsDate highlights={highlights} setSelectHighlights={setSelectHighlights}  />
+      <ul>
+      {Object.values(selectHighlights).map((highlightData) => {
         return (
-          <div>
-            <h3>{key}</h3>
-            <ul>
-              {value.map(v => <li>{v}</li>)}
-            </ul>
-          </div>
+          <li>{highlightData}</li>
         )
       })}
+      </ul>
     </>
   )
 }
